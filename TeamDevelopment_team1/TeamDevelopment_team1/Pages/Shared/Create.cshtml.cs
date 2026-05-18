3839b032627eb69d71b7727708bd7edd1b081c47
noma-taichi0301 <tnoma@youtheng.co.jp>
2026-05-18 15:37:15 +0900

    First Push

---
+++ TeamDevelopment_team1/TeamDevelopment_team1/Pages/Shared/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamDevelopment_team1.Pages.Shared
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TeamDevelopment_team1/TeamDevelopment_team1/Repositories/ITodoRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace TeamDevelopment_team1.Repositories
+{
+    public interface ITodoRepository
+    {
+    }
+}
