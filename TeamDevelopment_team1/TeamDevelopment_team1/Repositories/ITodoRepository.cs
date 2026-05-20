900e8d4a5c18a88d14d2cb81a2fa89d1e4fe3cfd
yuyanagano-youth <ynagano@youtheng.co.jp>
2026-05-20 10:27:50 +0900

    ITodoRepository.cs 作成

--- TeamDevelopment_team1/TeamDevelopment_team1/Repositories/ITodoRepository.cs
+++ TeamDevelopment_team1/TeamDevelopment_team1/Repositories/ITodoRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace TeamDevelopment_team1.Repositories
+﻿using TeamDevelopment_team1.Models;
+
+namespace TeamDevelopment_team1.Repositories;
+
+public interface ITodoRepository
 {
-    public interface ITodoRepository
-    {
-    }
+    List<TodoItem> GetAll(string? filter);
+
+    TodoItem? GetById(int id);
+
+    void Create(TodoItem todo);
+
+    void Update(TodoItem todo);
+
+    void ToggleStatus(int id);
+
+    void Delete(int id);
+
+    //(int Total, int Completed, int Overdue) GetStats();
 }
